563e35620680d6825102b46f844e04ebb43392cc
dinekoli <93979551+dinekoli@users.noreply.github.com>
2021-11-11 18:34:44 +0530

    Header Bar created
    Header Bar created
    
    Co-Authored-By: tirou95 <42536489+tirou95@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, StyleSheet, FlatList, Text, View, Alert, Platform } from 'react-native';
+import { AppRegistry, StyleSheet, FlatList, Text, View, Alert, Platform, Image, Button, Header, TouchableWithoutFeedback, Dimensions } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -8,7 +8,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 
 class HomeScreen extends Component {
- 
+
  constructor(props)
  {
    super(props);
@@ -25,7 +25,7 @@ class HomeScreen extends Component {
  }
 
  GetGridViewItem (item) {
-  
+
  Alert.alert(item);
 
  }
@@ -39,23 +39,23 @@ class HomeScreen extends Component {
 
 
 <View style={styles.MainContainer}>
- 
+
       <FlatList
-      
+
          data={ this.state.GridViewItems }
 
          renderItem={({item}) =><View style={styles.GridViewBlockStyle}>
 
             <Text style={styles.GridViewInsideTextItemStyle} onPress={this.GetGridViewItem.bind(this, item.key)} > {item.key} </Text>
-            
+
             </View>}
 
          numColumns={2}
 
         />
-   
-   
-</View>           
+
+
+</View>
    );
  }
 }
@@ -75,7 +75,8 @@ function App() {
         },
       }}>
         <Stack.Screen name="Home" component={HomeScreen} options={{
-          title: 'My home',
+          headerTitle: (props) => <HeaderComponent {...props} />,
+          title: 'Dashboard',
           headerTitleAlign: 'left',
           headerStyle: {
             backgroundColor: '#f4511e',
@@ -88,6 +89,28 @@ function App() {
         }}/>
       </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+function HeaderComponent() {
+  return (
+    <View style={{ height: 'auto', width: Dimensions.get('window').width*0.93, borderStyle: 'solid', borderWidth: 1, borderColor: '#0000ff', alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'row'}}>
+
+      <TouchableWithoutFeedback style={{flex: 2}} onPress={() => alert('This is profile image')}>
+        <Image source={require('./Resource/ReactNative-HouseImage/user.png')} style={styles.UserImage}  />
+      </TouchableWithoutFeedback>
+
+      <Text style={{ flex: 1, fontWeight: 'bold', fontSize: 20, fontWeight: '800'}}>Dashboard</Text>
+
+      <TouchableWithoutFeedback style={{flex: 1}} onPress={() => alert('This is bell image')}>
+        <Image source={require('./Resource/ReactNative-HouseImage/bell.png')} style={styles.BellImage}  />
+      </TouchableWithoutFeedback>
+
+      <TouchableWithoutFeedback style={{flex: 1}} onPress={() => alert('This is search image')}>
+        <Image source={require('./Resource/ReactNative-HouseImage/search.png')} style={styles.SearchImage}  />
+      </TouchableWithoutFeedback>
+
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -119,8 +142,24 @@ GridViewInsideTextItemStyle: {
    padding: 10,
    fontSize: 18,
    justifyContent: 'center',
-   
+
  },
+ UserImage: {
+   marginRight: 10,
+   width: 25,
+   height: 25
+ },
+ BellImage: {
+   right: 10,
+   width: 25,
+   height: 25
+ },
+ SearchImage: {
+   right: 0,
+   width: 20,
+   height: 20
+ }
+
 
 });
 
